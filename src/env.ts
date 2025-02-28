ba29172860298770c1d7a5d8b6a104c08a2367b6
JosiasNetto <josiascaldasnetto08@gmail.com>
2025-02-28 10:29:12 -0300

    Comentarios adcionados

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,7 @@
 import {z} from "zod";
 
+//Arquivo para manejar as váriaveis de ambiente do projeto
+
 const envSchema = z.object({
     PORT: z.coerce.number().default(3333),
     POSTGRES_URL: z.string().url(),
--- src/server.ts
+++ src/server.ts
@@ -3,46 +3,42 @@ import { fastifyCors } from "@fastify/cors" ;//Biblioteca de gerencicamento do f
 import {  validatorCompiler //Determina o formato de dados enviados pelo user
     , serializerCompiler, //Transformacao de dados ao enviar para o front end
     type ZodTypeProvider,
-    jsonSchemaTransform
+    jsonSchemaTransform //Transforma os Schema JSON para o swagger
  } from "fastify-type-provider-zod";
- import { z } from "zod";
- import { fastifySwagger } from "@fastify/swagger"
+ import { fastifySwagger } from "@fastify/swagger"  //Documentacao
  import { fastifySwaggerUi } from "@fastify/swagger-ui"
-import { subscripeToEventRoute } from "./routes/subscriEventRoute";
-import { env } from "./env";
-import { accessInviteLinkRoute } from "./routes/acessInviteLinkRoute";
+ import { env } from "./env";   //Importa o gerenciador de envs
+ import { accessInviteLinkRoute } from "./routes/acessInviteLinkRoute";
+ import { subscripeToEventRoute } from "./routes/subscriEventRoute";
 import { getSubscriberInviteClicksRoute } from "./routes/getSubscriberInviteClicksRoute";
 import { getSubscriberInviteCountRoute } from "./routes/getSubscriberInviteCountRoute";
 import { getSubscriberRankingPositionRoute } from "./routes/getSubscriberRankingPositionRoute";
 import { getRankingRoute } from "./routes/getRankingRoute";
 
-const app = fastify().withTypeProvider<ZodTypeProvider>();
+const app = fastify().withTypeProvider<ZodTypeProvider>();  //Atribui o servidor a app
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
 app.register(fastifyCors, {
-    origin: true,
+    origin: true,   //Define que qualquer origem pode fazer uma solicitacao ao servidor
 })
 
-app.register(fastifySwagger, {
+app.register(fastifySwagger, {  //Define a documentacao
     openapi: {
         info: {
             title: "NLW Connect",
             version: "0.0.1"
         }
     },
-    transform: jsonSchemaTransform
+    transform: jsonSchemaTransform 
 })
 
 app.register(fastifySwaggerUi, {
-    routePrefix: "/docs"
+    routePrefix: "/docs"    //Define a rota para o site da documentacao
 })
 
-app.get("/hello", () =>{
-    return "Hello world"
-})
-
+//Define as rotas
 app.register(subscripeToEventRoute); 
 app.register(accessInviteLinkRoute);
 app.register(getSubscriberInviteClicksRoute);
@@ -50,6 +46,7 @@ app.register(getSubscriberInviteCountRoute);
 app.register(getSubscriberRankingPositionRoute);
 app.register(getRankingRoute);
 
+//Executa o servidor
 app.listen({ port : env.PORT }).then(() =>{
     console.log("HTTP server runing...")
 })
